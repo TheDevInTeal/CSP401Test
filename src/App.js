5b8408ab5f99ffed71f26ab2ff9a259d23f829ef
TheDevInTeal <116689293+TheDevInTeal@users.noreply.github.com>
2022-10-25 18:57:57 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>CSP401 CNet Review</h1>
+      </header>
+      <body>
+        <h2>Start editing to see some magic happen!</h2>
+      </body>
+    </div>
+  );
+}
